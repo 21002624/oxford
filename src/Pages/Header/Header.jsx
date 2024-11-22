01911f1e6f37a90f331c5752d6569588cb20f4e9
21002624 <akasharul2407@gmail.com>
2024-11-22 14:12:03 +0530

    mobile nav

--- src/Pages/Header/Header.jsx
+++ src/Pages/Header/Header.jsx
@@ -6,34 +6,29 @@ import { Menu as MenuIcon, Close as CloseIcon } from '@mui/icons-material';
 import MobNav from '../../Components/MobNav/MobNav';
 
 const Header = () => {
-  // State to toggle mobile menu visibility
   const [isMobNavVisible, setIsMobNavVisible] = useState(false);
-
-  // Function to toggle MobNav visibility
   const ShowMobNav = () => {
     setIsMobNavVisible(!isMobNavVisible);
   };
 
   return (
     <header>
-      {/* Mobile Navigation */}
+
       <div className="headerLogo">
         <div>
           oxford.in
           <p className="logoDis">thiruninravur</p>
         </div>
         <div className="mobMenuIcon" onClick={ShowMobNav}>
-          {/* Toggle between Menu and Close icon */}
           {isMobNavVisible ? <CloseIcon /> : <MenuIcon />}
         </div>
       </div>
 
-      {/* Mobile Nav items, Slide down when clicked */}
+
       <div className={`mobNavDiv ${isMobNavVisible ? 'visible' : ''}`}>
         <MobNav />
       </div>
 
-      {/* Header Left Section */}
       <div className="headerLeft">
         <div className="headerSearch">
           <Nav />
