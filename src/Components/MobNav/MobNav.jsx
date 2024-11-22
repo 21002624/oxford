4d5e90ca0fa0b6f65255fc31cb561f8ba8c04c43
21002624 <akasharul2407@gmail.com>
2024-11-22 14:08:14 +0530

    mobile nav

---
+++ src/Components/MobNav/MobNav.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './MobNav.css'; 
+
+
+const MobNav = () => {
+  return (
+    <div>
+      <ul className="mobNav">
+        <li className="mobNavItem">
+          <Link to="/">Home</Link>
+        </li>
+        <li className="mobNavItem">
+          <Link to="/about">About</Link> 
+        </li>
+        <li className="mobNavItem">
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default MobNav
--- src/Pages/Header/Header.jsx
+++ src/Pages/Header/Header.jsx
@@ -1,20 +1,42 @@
 import React, { useState } from 'react';
 import './Header.css';
-import ModernSearchInput from '../../Components/Search/Search';
 import CustomizedSwitches from '../../Components/DarkMode/DarkMode';
 import Nav from '../../Components/Nav/Nav';
+import { Menu as MenuIcon, Close as CloseIcon } from '@mui/icons-material';
+import MobNav from '../../Components/MobNav/MobNav';
 
 const Header = () => {
+  // State to toggle mobile menu visibility
+  const [isMobNavVisible, setIsMobNavVisible] = useState(false);
+
+  // Function to toggle MobNav visibility
+  const ShowMobNav = () => {
+    setIsMobNavVisible(!isMobNavVisible);
+  };
 
   return (
     <header>
+      {/* Mobile Navigation */}
       <div className="headerLogo">
-        oxford.in
-        <p className='logoDis'>thiruninravur</p>
+        <div>
+          oxford.in
+          <p className="logoDis">thiruninravur</p>
+        </div>
+        <div className="mobMenuIcon" onClick={ShowMobNav}>
+          {/* Toggle between Menu and Close icon */}
+          {isMobNavVisible ? <CloseIcon /> : <MenuIcon />}
+        </div>
       </div>
+
+      {/* Mobile Nav items, Slide down when clicked */}
+      <div className={`mobNavDiv ${isMobNavVisible ? 'visible' : ''}`}>
+        <MobNav />
+      </div>
+
+      {/* Header Left Section */}
       <div className="headerLeft">
         <div className="headerSearch">
-        <Nav />
+          <Nav />
         </div>
         <div className="headerIcons">
           <CustomizedSwitches />
